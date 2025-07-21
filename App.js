e653a5a6dc992ce1cbc59872c1b1e4e90d9cf102
VidyaChandrasekar <47977555+VidyaChandrasekar@users.noreply.github.com>
2025-07-21 15:32:25 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Tree from "./Tree"; // ✅ CORRECT: Default import
+
+function App() {
+  return (
+    <div className="App">
+      <Tree />
+    </div>
+  );
+}
+
+export default App;
